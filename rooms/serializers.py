eb7bffd06ddf5d2da2db83b25189859b834443e7
Youngjin Jo <yjinjo@berkeley.edu>
2023-05-12 07:48:15 +0900

    #2. Room 모델의 serializers에서 superhost 여부 링크

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 
+from rooms.models import Room
+from users.serializers import TinyUserSerializer
 
-class RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
+
+class RoomSerializer(serializers.ModelSerializer):
+    user = TinyUserSerializer()
+
+    class Meta:
+        model = Room
+        fields = ("name", "price", "instant_book", "user")
