3b93f1f432a02dca5348d4fb862c32707e2974b7
Youngjin Jo <yjinjo@berkeley.edu>
2023-05-12 08:26:11 +0900

    #2. RetrieveAPIView를 이용한 특정 pk Room의 상세 페이지 구현

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,4 +6,5 @@ app_name = "rooms"
 
 urlpatterns = [
     path("list/", views.ListRoomsView.as_view()),
+    path("<int:pk>/", views.SeeRoomView.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,16 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from rooms.models import Room
-from rooms.serializers import RoomSerializer
+from rooms.serializers import RoomSerializer, BigRoomSerializer
 
 
 class ListRoomsView(ListAPIView):
 
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class SeeRoomView(RetrieveAPIView):
+
+    queryset = Room.objects.all()
+    serializer_class = BigRoomSerializer
