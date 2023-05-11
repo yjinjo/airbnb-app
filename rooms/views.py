c658debb40489b3f384615dc3ccb74b9171dae88
Youngjin Jo <yjinjo@berkeley.edu>
2023-05-11 21:01:28 +0900

    #2. DRF serializers를 통해 json 변환 및 Model에서 원하는 필드만 api에 노출

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from rooms.models import Room
+from rooms.serializers import RoomSerializer
+
 
 @api_view(["GET"])
 def list_rooms(request):
-    return Response()
+    rooms = Room.objects.all()
+    serialized_rooms = RoomSerializer(rooms, many=True)
+    return Response(data=serialized_rooms.data)
