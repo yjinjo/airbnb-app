668b1b67c4429e66208225c40ef56a2560ad5d62
Youngjin Jo <yjinjo@berkeley.edu>
2023-05-12 10:42:20 +0900

    #3. FBV --> CBV로 Refactoring 및 특정 Room 상세 정보 응답
    - [x] Detail Room

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,6 +5,6 @@ from rooms import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("", views.rooms_view),
-    path("<int:pk>/", views.SeeRoomView.as_view()),
+    path("", views.RoomsView.as_view()),
+    path("<int:pk>/", views.RoomView.as_view()),
 ]
