3d38e3589bc608cf2de3aede5eb1b538cecb2bb4
Youngjin Jo <yjinjo@berkeley.edu>
2023-05-13 20:03:37 +0900

    #4. ModelViewSet 이용하여 Room 즐겨찾기 목록 코드 구현

--- users/views.py
+++ users/views.py
@@ -3,9 +3,8 @@ from django.conf import settings
 from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from rooms.models import Room
@@ -51,18 +50,19 @@ class UsersViewSet(ModelViewSet):
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-
-class FavsView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user = request.user
-        serializer = RoomSerializer(user.favs.all(), many=True).data
+    # detail=True := /users/pk, detail=False := /users/
+    @action(detail=True)
+    def favs(self, request, pk):
+        user = self.get_object()
+        serializer = RoomSerializer(
+            user.favs.all(), many=True, context={"request": request}
+        ).data
         return Response(serializer)
 
-    def put(self, request):
+    @favs.mapping.put
+    def toggle_favs(self, request, pk):
         pk = request.data.get("pk", None)
-        user = request.user
+        user = self.get_object()
         if pk:
             try:
                 room = Room.objects.get(pk=pk)
